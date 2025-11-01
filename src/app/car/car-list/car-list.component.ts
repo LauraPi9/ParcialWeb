f3b23ea408c9bf90093e6b1efc02a78747d7a492
LauraP <l.pinzonm2@uniandes.edu.co>
2025-10-31 19:26:26 -0500

    feat(Punto2):  :zap: list brands

--- src/app/car/car-list/car-list.component.ts
+++ src/app/car/car-list/car-list.component.ts
@@ -6,22 +6,46 @@ import { CarService } from '../car.service';
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
+  styleUrls: ['./car-list.component.css'],
 })
 export class CarListComponent implements OnInit {
   cars: Array<Car> = [];
-  constructor(private carService: CarService) { }
+  brandCounts: Record<string, number> = {};
+  brandCountsArray: Array<{ brand: string; count: number }> = [];
+  constructor(private carService: CarService) {}
 
-
-  getCars(){
+  getCars() {
     this.carService.getCars().subscribe((cars) => {
       this.cars = cars;
+      this.computeBrandCounts();
     });
   }
-
 
   ngOnInit(): void {
     this.getCars();
   }
 
+  private computeBrandCounts() {
+    this.brandCounts = {};
+    for (const car of this.cars) {
+      const carBrand = (car.marca || '').toString();
+      if (!carBrand) {
+        continue;
+      }
+
+      this.brandCounts[carBrand] = (this.brandCounts[carBrand] || 0) + 1;
+    }
+
+    console.log({brandCounts: this.brandCounts});
+    console.log({brandCounts:  Object.keys(this.brandCounts)});
+
+    this.brandCountsArray = Object.keys(this.brandCounts).map(
+      (currentBrand) => ({
+        brand: currentBrand,
+        count: this.brandCounts[currentBrand],
+      })
+    );
+  }
+
+  
 }
